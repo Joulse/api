2dd7876c138919ad5e3fef61ca57a940284854cc
Julien <julien.ducrot@fashiongroup.com>
2015-12-30 15:30:17 +0100

    docs(readme): update readme and experiences

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var data = {};
 data.name = 'Joulse';
 data.username = 'Joulse';
 data.title = 'Front End Developer';
-data.byline = 'Front End Developer at Attractrive World. Based in Paris, France.';
+data.byline = 'Front End Developer at Fashion Group. Based in Paris, France.';
 data.homepage = '//joulse.com';
 
 data.social_networks = [
-    { name: 'Twitter', link: '//twitter.com/joulse' },
+    { name: 'Twitter', link: '//twitter.com/joulse_' },
     { name: 'Github', link: '//github.com/joulse' },
     { name: 'Soundcloud', link: '//soundcloud.com/joulse' }
 ];
@@ -17,13 +17,27 @@ data.social_networks = [
 data.projects = {
     featured: [
         {
+            name: 'Fashion Group', link: '//fashiongroup.com',
+            description: 'The world leader in online services for fashion professionals.
+                Fashion Group operates FashionJobs.com, FashionMag.com and FashionNetwork.com
+                around the world available in 10 languages. '
+        },
+
+        {
+            name: 'Attractive World', link: '//attractiveworld.net',
+            description: 'Attractive World is a web dating site that was the first to propose a principle
+                of selective admission.'
+        },
+
+        {
             name: 'SensCritique', link: '//senscritique.com',
-            description: 'SensCritique lets you discover, note and share movies, series, books, comics, games and music'
+            description: 'SensCritique lets you discover, note and share movies, series, books, comics, games and music.'
         },
 
         {
             name: 'HereWeStyle', link: '//herewestyle.com',
-            description: 'HereWeStyle is a personalized online styling service. Our professional stylists will find your style according to your body, your taste and personality.'
+            description: 'HereWeStyle is a personalized online styling service. Our professional stylists
+                will find your style according to your body, your taste and personality.'
         },
 
         {
